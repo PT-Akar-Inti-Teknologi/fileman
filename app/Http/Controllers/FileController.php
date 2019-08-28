07f10e48dda52e3f18e5adfa119f09830c6e8f65
inspira-raina <raina@inspira.web.id>
2019-08-28 23:02:41 +0700

    First Commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FileController extends Controller
+{
+    public function getFile($uuid)
+    {
+        $data = request()->session()->all();
+        return $data;
+        // $file = DB::table('application_files')->select('file_path')->where('uuid', $uuid)->first();
+        // $path = storage_path('app/'.$file->file_path);
+        // // return response()->download($path);
+        // return response()->json(['message'=>$file], 200);
+
+    }
+
+    public function postFile(Request $request)
+    {
+        $file = $request->file('attachment');
+        $folder = $request->input('ApplicationID');
+        $extension = $file->getClientOriginalExtension();
+        $attachment = $request->input('filename').'.'.$extension;
+        $storagepath = $file->move(storage_path('app/attachment/'.$folder.'/'), $attachment);
+        $fileurl = url('/'.$attachment);
+        return response()->json(['url' => $fileurl], 200);
+    }
+}
