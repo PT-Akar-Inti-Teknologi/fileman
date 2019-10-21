18573c28f60ca71f2f747c0cf1ef1286fb69a9ea
inspira-raina <raina@inspira.web.id>
2019-10-21 14:14:51 +0700

    File upload service

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,16 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\ApplicationFile;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-    public function getFile(Request $request)
+    public function getFile($uuid)
     {
-        $path = $request->path;
-        return Storage::download($path);
+        $file = DB::table('application_files as A')
+        ->leftjoin('applications as B', 'B.id', '=', 'A.application_id')
+        ->where('A.uuid', $uuid)
+        ->select(
+            'A.file_name as name',
+            'A.file_path as path',
+            'B.cst_name as customer',
+            )
+        ->first();
+        $name = strtoupper($file->customer.' - '.$file->name);
+        $extn = pathinfo($file->path, PATHINFO_EXTENSION);
+        $path = storage_path('app/public/attachment/'.$file->path);
+        $headers = [
+            'Content-Type' => mime_content_type($path),
+        ];
+        return Storage::download($path, $name.'.'.$extn, $headers);
     }
 
     public function postFile(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:api')->group(function() {
-    Route::get('file/download', 'FileController@getFile');
+    Route::get('file/download/{uuid}', 'FileController@getFile');
     Route::post('file/upload', 'FileController@postFile');
 });
